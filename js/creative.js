095b7b39c07946faf8098e6926b5f2802b1d403d
Tyler Fletcher <tfletcher@sastampabay.org>
2016-06-27 12:56:07 -0400

    Finished Price Calc

--- js/creative.js
+++ js/creative.js
@@ -1,94 +1,111 @@
 (function ($) {
     "use strict"; // Start of use strict
 
-    // Get the modal
-    var modal = document.getElementById('myModal');
 
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
+// Get the modal
+var modal = document.getElementById('myModal');
 
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-    // When the user clicks on the button, open the modal
-    btn.onclick = function () {
-        modal.style.display = "block";
-        var price = checkPrices();
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-        if (isNaN(price)) {
-            $('#priceHeader').hide();
-            document.getElementById("priceInput").innerHTML = price.toString();
-        } else {
-            document.getElementById("priceInput").innerHTML = "$" + price.toString();
-        }
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+    modal.style.display = "block";
+    var price = checkPrices();
+    var petNum = document.getElementById("petValue").value;
+    var visitNum = document.getElementById("visitValue").value;
+    var visitTypeNum = document.getElementById("visitTypeValue").value;
+    var visitType;
+
+    if (visitTypeNum == 1) {
+        visitType = "Regular";
+    } else if (visitTypeNum == 2) {
+        visitType = "Quick";
+    } else {
+        visitType = "Extended";
     }
 
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function () {
+    if (isNaN(price)) {
+        $('#priceHeader').hide();
+        document.getElementById("priceInput").innerHTML = price.toString();
+    } else {
+        $('#priceHeader').show();
+        document.getElementById("priceInput").innerHTML = "$" + price.toString();
+        document.getElementById("numOfPets").innerHTML = petNum;
+        document.getElementById("numOfVisits").innerHTML = visitNum;
+        document.getElementById("typeOfVisit").innerHTML = visitType;
+    }
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
         modal.style.display = "none";
     }
+}
 
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
+
+
+
+function checkPrices() {
+    var petNum = document.getElementById("petValue").value;
+    var visitNum = document.getElementById("visitValue").value;
+    var visitTypeNum = document.getElementById("visitTypeValue").value;
+    var totalPrice = 0;
+    var basePrice = 0;
+    var additionalPetPrice = 0;
+    var discount = 0;
+
+    if (visitTypeNum == 1) {
+        basePrice = 20;
+    } else if (visitTypeNum == 2) {
+        basePrice = 14;
+    } else {
+        basePrice = 30;
+    }
+
+    if (petNum == 5) {
+        return "Wow thats A Lot Of Pets!<br>Please Contact Us For Pricing!";
+    } else if (visitNum == 20) {
+        return "Wow thats A Lot Of Visits!<br>Please Contact Us To Help You Schedule!";
+    } else {
+        if (petNum > 1) {
+            if (visitNum > 10 && visitNum < 20) {
+                // 10% OFf
+                discount = (basePrice * visitNum) * 0.10;
+                additionalPetPrice = (petNum - 1) * 10;
+                totalPrice = ((basePrice + additionalPetPrice) * visitNum) - discount;
+                return totalPrice;
+
+            } else {
+                additionalPetPrice = (petNum - 1) * 10;
+                totalPrice = ((basePrice + additionalPetPrice) * visitNum);
+                return totalPrice;
+            }
+        } else {
+            if (visitNum >= 10 && visitNum < 20) {
+                // 10% OFf
+                discount = (basePrice * visitNum) * 0.10;
+                totalPrice = (basePrice * visitNum) - discount;
+                return totalPrice;
+
+            } else {
+                totalPrice = basePrice * visitNum;
+                return totalPrice;
+            }
         }
     }
 
-
-
-
-    function checkPrices() {
-        var petNum = document.getElementById("petValue").value;
-        var visitNum = document.getElementById("visitValue").value;
-        var visitTypeNum = document.getElementById("visitTypeValue").value;
-        var totalPrice = 0;
-        var basePrice = 0;
-        var additionalPetPrice = 0;
-        var discount = 0;
-
-        if (visitTypeNum == 1) {
-            basePrice = 20;
-        } else if (visitTypeNum == 2) {
-            basePrice = 14;
-        } else {
-            basePrice = 30;
-        }
-
-        if (petNum == 5) {
-            return "Wow thats A Lot Of Pets!<br>Please Contact Us For Pricing!";
-        } else if (visitNum == 20) {
-            return "Wow thats A Lot Of Visits!<br>Please Contact Us To Help You Schedule!";
-        } else {
-            if (petNum > 1) {
-                if (visitNum >= 10 && visitNum < 20) {
-                    // 10% OFf
-                    discount = (basePrice * visitNum) * 0.10;
-                    additionalPetPrice = (petNum - 1) * 10;
-                    totalPrice = ((basePrice + additionalPetPrice) * visitNum) - discount;
-                    return totalPrice;
-
-                } else {
-                    additionalPetPrice = (petNum - 1) * 10;
-                    totalPrice = ((basePrice + additionalPetPrice) * visitNum);
-                    return totalPrice;
-                }
-            } else {
-                if (visitNum >= 10 && visitNum < 20) {
-                    // 10% OFf
-                    discount = (basePrice * visitNum) * 0.10;
-                    totalPrice = (basePrice * visitNum) - discount;
-                    return totalPrice;
-
-                } else {
-                    totalPrice = basePrice * visitNum;
-                    return totalPrice;
-                }
-            }
-        }
-
-        return false;
-    };
+    return false;
+};
 
 
 
