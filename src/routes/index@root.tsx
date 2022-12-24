6eb0545b8a472c84b800880c061df36f88d94793
Tyler Fletcher <fletchertyler914@yahoo.com>
2022-12-24 11:37:02 -0600

    Test animations

--- src/routes/index@root.tsx
+++ src/routes/index@root.tsx
@@ -2,13 +2,19 @@ import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
+  const headings = ['All.', 'Things.', 'Living.'];
+
   return (
-    <div class='p-4'>
-      <h1 class={'prose prose-lg text-8xl'}>
-        All. <br />
-        Things. <br />
-        Living. <br />
-      </h1>
+    <div class='p-4 prose prose-lg text-2xl flex flex-col gap-2'>
+      {headings.map((title, index) => (
+        <h1
+          class={`animate-fade-right animate-once animate-duration-1000 animate-delay-[${
+            index * 400
+          }ms] animate-ease-in-out animate-fill-backwards m-0 p-0`}
+        >
+          {title}
+        </h1>
+      ))}
     </div>
   );
 });
