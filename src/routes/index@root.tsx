f1fdbfe587752529f57155e945e15abc752727a8
Tyler Fletcher <fletchertyler914@yahoo.com>
2022-12-24 19:44:27 -0600

    Ole tailwind...

--- src/routes/index@root.tsx
+++ src/routes/index@root.tsx
@@ -2,19 +2,29 @@ import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
-  const headings = ['All.', 'Things.', 'Living.'];
-
   return (
     <div class='p-4 prose prose-lg text-2xl flex flex-col gap-2'>
-      {headings.map((title, index) => (
-        <h1
-          class={`animate-fade-right animate-once animate-duration-1000 animate-delay-[${
-            index * 400
-          }ms] animate-ease-in-out animate-fill-backwards m-0 p-0`}
-        >
-          {title}
-        </h1>
-      ))}
+      <h1
+        class={
+          'animate-fade-right animate-once animate-duration-1000 animate-delay-[400ms] animate-ease-in-out animate-fill-backwards m-0 p-0'
+        }
+      >
+        All.
+      </h1>
+      <h1
+        class={
+          'animate-fade-right animate-once animate-duration-1000 animate-delay-[800ms] animate-ease-in-out animate-fill-backwards m-0 p-0'
+        }
+      >
+        Things.
+      </h1>
+      <h1
+        class={
+          'animate-fade-right animate-once animate-duration-1000 animate-delay-[1200ms] animate-ease-in-out animate-fill-backwards m-0 p-0'
+        }
+      >
+        Living.
+      </h1>
     </div>
   );
 });
